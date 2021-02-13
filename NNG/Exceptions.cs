6972a525c0fd05b83a708edb884719bdcf264685
LeeHastings <lee.hastings@outlook.com>
2021-02-12 17:37:01 -0800

    Tried to improve, gave up

---
+++ NNG/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNG
+{
+    class GeneratorException : Exception
+    {
+        public GeneratorException(string file, string context)
+            : base(string.Format("Error in {0}: {1}", file, context))
+        { }
+    }
+}
---
+++ NNG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generator gen = new Generator("src", "www");
+            gen.GenerateAllFiles();
+        }
+    }
+}
